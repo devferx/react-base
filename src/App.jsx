f5525e302b6ea083dcf708da4f203d4e64d67847
devferx <ferkki@hotmail.com>
2020-11-30 15:38:11 -0400

    [fix] added css-loader dependencie

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './App.css'
 
 const App = () => {
+  const [counter, setCounter] = useState(0)
   return (
     <div>
       <h1>Hola desde mi app!</h1>
+      <button onClick={() => {setCounter(counter + 1)}}>{ counter }</button>
     </div>
   )
 }
